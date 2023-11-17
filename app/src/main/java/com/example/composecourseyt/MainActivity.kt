9cd9116b21213c73bdabc7370e98f1f07cf45b0e
Bob <113605310+BobHogvall@users.noreply.github.com>
2023-11-17 15:35:04 +0100

    Add code for imageBox

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -19,6 +20,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -29,6 +33,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -46,7 +54,7 @@ class MainActivity : ComponentActivity() {
                 val color = remember {
                     mutableStateOf(Color.Yellow)
                 }
-                Text("Let's click on some stuff",
+                Text("Welcome!",
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier
@@ -64,9 +72,16 @@ class MainActivity : ComponentActivity() {
                     .weight(1f)
                     .fillMaxSize()
                 )
-
-
+                }
+            val painter  = painterResource(id = R.drawable.img)
+            val description = "a bunch of daisies"
+            val title = "daisies"
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)) {
+                ImageCard (painter = painter, contentDescription = description, title = title)
             }
+
         }
     }
 }
@@ -107,5 +122,34 @@ fun ColorBox(
             }
         }
     }
+}
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card (
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+
+    ){
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart){
+                Text(
+                    title, style = TextStyle(color = Color.White, fontSize = 16.sp)
+                )
+            }
+        }
+    }
 }
