55767c137ebd70e470d4e6215184798f68313033
Bob <113605310+BobHogvall@users.noreply.github.com>
2023-11-21 11:54:45 +0100

    Add progressBar component

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -29,21 +33,34 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.isTraceInProgress
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
 import kotlin.random.Random
 
@@ -51,106 +68,94 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(Modifier.height(300.dp), verticalArrangement = Arrangement.Bottom){
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-                Text("Welcome!",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .padding(16.dp))
+            val constraints = ConstraintSet {
+                val mondayBox = createRefFor("mondaybox")
+                val tuesdayBox = createRefFor("tuesdaybox")
+                val progressBarBox = createRefFor("progressbarbox")
+                createVerticalChain(progressBarBox, mondayBox, tuesdayBox)
 
-                ColorBox(
-                    Modifier
-                        .weight(1f)
-                        .fillMaxSize()
-                ) {
-                    color.value = it
+                constrain(progressBarBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start, margin = 40.dp) //fix how to center, perhaps use guideline
+                    width = Dimension.value(300.dp)
+                    height = Dimension.value(300.dp)
                 }
+                constrain(mondayBox){
+                    top.linkTo(progressBarBox.bottom)
+                    start.linkTo(progressBarBox.start)
+                    width = Dimension.value(300.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                constrain(tuesdayBox){
+                    top.linkTo(mondayBox.bottom)
+                    start.linkTo(mondayBox.start)
+                    width = Dimension.value(300.dp)
+                    height = Dimension.value(100.dp)
+                }
+            }
+            ConstraintLayout (constraints, modifier = Modifier.fillMaxSize()) {
                 Box(modifier = Modifier
-                    .background(color.value)
-                    .weight(1f)
-                    .fillMaxSize()
-                )
+                    .background(Color.LightGray)
+                    .layoutId("progressbarbox"))
+                Box(modifier = Modifier
+                    .background(Color.Green)
+                    .layoutId("mondaybox"))
+                Box(modifier = Modifier
+                    .background(Color.Red)
+                    .layoutId("tuesdaybox"),
+                contentAlignment = Alignment.Center){
+                    Text (
+                        text = "Tuesday"
+                    )
                 }
-            val painter  = painterResource(id = R.drawable.img)
-            val description = "a bunch of daisies"
-            val title = "daisies"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)) {
-                ImageCard (painter = painter, contentDescription = description, title = title)
-            }
-
-        }
-    }
-}
-
-@Composable
-fun ColorBox(
-    modifier: Modifier = Modifier,
-    updateColor: (Color) -> Unit) {
-
-    Box(modifier = modifier
-        .background(Color.Red)
-        .clickable {
-            updateColor(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            )
-        }) {
-        LazyColumn{
-            itemsIndexed(
-                listOf("Press", "this")
-            ) { index, string ->
-                Text(
-                    text = string,
-                    fontSize = 32.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp)
-
-                )
-
             }
         }
     }
 }
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card (
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
 
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+    ) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        ), label = "function to animate from old to new value"
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius*2f)
     ){
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
+        Canvas(modifier = Modifier.size(radius*2)){
+            drawArc(
+                color = color,
+                -90f,
+                360*curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
             )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-                contentAlignment = Alignment.BottomStart){
-                Text(
-                    title, style = TextStyle(color = Color.White, fontSize = 16.sp)
-                )
-            }
         }
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold)
     }
 }
