dcbde0b38fbc3cc07393ec2442001b743b490334
Bob <113605310+BobHogvall@users.noreply.github.com>
2023-11-23 13:33:23 +0100

    add NavigationBar to bottom of home screen

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -5,7 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -16,15 +25,49 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
+import androidx.navigation.compose.rememberNavController
+import com.example.composecourseyt.composables.BottomNavigationBar
 import com.example.composecourseyt.composables.CircularProgressBar
 import com.example.composecourseyt.composables.Navigation
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(color = Color(0xFF202020), modifier = Modifier.fillMaxSize()){
-                Navigation()
+            val navController = rememberNavController()
+            Scaffold(
+                bottomBar = {
+                    BottomNavigationBar(
+                        items = listOf(
+                            BottomNavItem(
+                                name = "Home",
+                                route = "main_screen",
+                                icon = Icons.Default.Home
+                            ),
+                            BottomNavItem(
+                                name = "Chat",
+                                route = "chat",
+                                icon = Icons.Default.Notifications,
+                                badgeCount = 13
+                            ),
+                            BottomNavItem(
+                                name = "Settings",
+                                route = "settings",
+                                icon = Icons.Default.Settings
+                            ),
+                        ),
+                        navController = navController,
+                        onItemCLick = {
+                            navController.navigate(it.route)
+                        })
+                }
+            ) { innerPadding ->
+                Column(modifier = Modifier.padding(innerPadding)) {
+                    Surface(color = Color(0xFF202020), modifier = Modifier.fillMaxSize()) {
+                        Navigation(navController = navController)
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/composecourseyt/composables/Screens.kt
+++ app/src/main/java/com/example/composecourseyt/composables/Screens.kt
@@ -1,16 +1,26 @@
 package com.example.composecourseyt.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
@@ -18,113 +28,208 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.composecourseyt.BottomNavItem
 
 @Composable
-fun Navigation() {
-    val navController = rememberNavController()
+fun Navigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "splash_screen") {
-        composable("splash_screen"){
+        composable("splash_screen") {
             SplashScreen(navController = navController)
         }
-        composable("main_screen"){
+        composable("main_screen") {
             MainScreen()
+        }
+        composable("chat") {
+            ChatScreen()
+        }
+        composable("settings") {
+            SettingsScreen()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemCLick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    NavigationBar(
+        modifier = modifier,
+        containerColor = Color.Black,
+        tonalElevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            NavigationBarItem(
+                selected = selected,
+                onClick = { onItemCLick(item) },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Green,
+                    unselectedIconColor = Color.LightGray
+                ),
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        if (item.badgeCount > 0) { //badgedBox is experimental => may be changed in the future
+                            BadgedBox(badge = { Badge { Text(item.badgeCount.toString()) } }) {
+                                Icon(imageVector = item.icon, contentDescription = item.name)
+                            }
+                        } else {
+                            Icon(imageVector = item.icon, contentDescription = item.name)
+                        }
+                        if (selected) {
+                            Text(text = item.name, textAlign = TextAlign.Center, fontSize = 10.sp)
+                        }
+                    }
+                })
         }
     }
 }
 
 @Composable
-fun SplashScreen(navController: NavController){
+fun ChatScreen() {
     Box(
-       contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ){
-        CircularProgressBar(percentage = 1f, number = 100, navController = navController )
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Chat Screen")
     }
 }
 
 @Composable
-fun MainScreen(){
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Settings Screen")
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressBar(percentage = 1f, number = 100, navController = navController)
+    }
+}
+
+@Composable
+fun MainScreen() {
     val constraints = ConstraintSet {
         val mondayBox = createRefFor("mondaybox")
         val tuesdayBox = createRefFor("tuesdaybox")
         val wednesdayBox = createRefFor("wednesdaybox")
         val thursdayBox = createRefFor("thursdaybox")
         val fridayBox = createRefFor("fridaybox")
-        createVerticalChain(mondayBox, tuesdayBox, wednesdayBox, thursdayBox, fridayBox, chainStyle = ChainStyle.Spread)
+        createVerticalChain(
+            mondayBox,
+            tuesdayBox,
+            wednesdayBox,
+            thursdayBox,
+            fridayBox,
+            chainStyle = ChainStyle.Spread
+        )
 
-        constrain(mondayBox){
+        constrain(mondayBox) {
             top.linkTo(parent.top)
-            start.linkTo(parent.start, margin = 48.dp) //better way to center? Might be better with column.
+            start.linkTo(
+                parent.start,
+                margin = 48.dp
+            ) //better way to center? Might be better with column.
             width = Dimension.percent(0.75f)
             height = Dimension.value(50.dp)
         }
-        constrain(tuesdayBox){
+        constrain(tuesdayBox) {
             top.linkTo(mondayBox.bottom)
             start.linkTo(mondayBox.start)
             width = Dimension.percent(0.75f)
             height = Dimension.value(50.dp)
         }
-        constrain(wednesdayBox){
+        constrain(wednesdayBox) {
             top.linkTo(tuesdayBox.bottom)
             start.linkTo(tuesdayBox.start)
             width = Dimension.percent(0.75f)
             height = Dimension.value(50.dp)
         }
-        constrain(thursdayBox){
+        constrain(thursdayBox) {
             top.linkTo(wednesdayBox.bottom)
             start.linkTo(wednesdayBox.start)
             width = Dimension.percent(0.75f)
             height = Dimension.value(50.dp)
         }
-        constrain(fridayBox){
+        constrain(fridayBox) {
             top.linkTo(thursdayBox.bottom)
             start.linkTo(thursdayBox.start)
             width = Dimension.percent(0.75f)
             height = Dimension.value(50.dp)
         }
     }
-    ConstraintLayout (constraints, modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier
-            .background(Color.Green)
-            .layoutId("mondaybox"),
-            contentAlignment = Alignment.Center){
+    ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId("mondaybox"),
+            contentAlignment = Alignment.Center
+        ) {
             Text(
                 text = "Monday",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
         }
-        Box(modifier = Modifier
-            .background(Color.Red)
-            .layoutId("tuesdaybox"),
-            contentAlignment = Alignment.Center){
-            Text (text = "Tuesday",
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .layoutId("tuesdaybox"),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Tuesday",
                 fontSize = 24.sp,
-                fontWeight = FontWeight.Bold)
+                fontWeight = FontWeight.Bold
+            )
         }
-        Box(modifier = Modifier
-            .background(Color.LightGray)
-            .layoutId("wednesdaybox"),
-            contentAlignment = Alignment.Center){
-            Text(text = "Wednesday",
+        Box(
+            modifier = Modifier
+                .background(Color.LightGray)
+                .layoutId("wednesdaybox"),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Wednesday",
                 fontSize = 24.sp,
-                fontWeight = FontWeight.Bold)
+                fontWeight = FontWeight.Bold
+            )
         }
-        Box(modifier = Modifier
-            .background(Color.Magenta)
-            .layoutId("thursdaybox"),
-            contentAlignment = Alignment.Center){
-            Text(text = "Thursday",
+        Box(
+            modifier = Modifier
+                .background(Color.Magenta)
+                .layoutId("thursdaybox"),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Thursday",
                 fontSize = 24.sp,
-                fontWeight = FontWeight.Bold)
+                fontWeight = FontWeight.Bold
+            )
         }
-        Box(modifier = Modifier
-            .background(Color.Yellow)
-            .layoutId("fridaybox"),
-            contentAlignment = Alignment.Center){
+        Box(
+            modifier = Modifier
+                .background(Color.Yellow)
+                .layoutId("fridaybox"),
+            contentAlignment = Alignment.Center
+        ) {
             Text(text = "Friday", fontSize = 26.sp, fontWeight = FontWeight.Bold)
         }
     }
