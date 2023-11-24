3715989bee67f6a0ecc9fd2ca802031d9f872de1
Bob <113605310+BobHogvall@users.noreply.github.com>
2023-11-24 09:42:29 +0100

    change HomeScreen to MainScreen

---
+++ app/src/main/java/com/example/composecourseyt/composables/GreetingSection.kt
@@ -0,0 +1,51 @@
+package com.example.composecourseyt.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GreetingSection(
+    name: String = "Bob"
+){
+    Row(
+       horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ){
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good morning, $name",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = "We wish you have a good day",
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        Icon(
+            Icons.Rounded.Search,
+            contentDescription = "search function",
+            tint = Color.LightGray,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
